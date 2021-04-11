a09526cdcb1f0227f4dd93b2fcbe95abf79bd746
liad <liad142@gmail.com>
2021-04-11 13:21:59 +0300

    comiitng w2d1

---
+++ week2/day1/week2day1.js
@@ -0,0 +1,23 @@
+let addressNumber = 14
+let addressStreet = 'Ussishkin'
+let country = 'israel'
+let global_address = 'I live in '+addressStreet + '' + addressNumber + '' + country
+/////////
+let birth = 1994
+let future=2030
+let age = birth + ' ' - future
+let sentence = 'i will be ' + age + ' in ' + future
+///
+// EXERSICE XP GOLD
+let me = ["my","favorite","color","is","blue"]
+let newMe = me.toString()
+//
+let str1 = "mix"
+let str2 = "pod"
+let newWorld = str1.replace('mix','mio')
+let newWorlds = str2.replace('pod','pid')
+//
+let num1 = prompt('please enter a number')
+let num2 = prompt('please enter another number')
+alert(num1 + num2)
+
